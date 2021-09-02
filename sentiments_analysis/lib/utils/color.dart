7fa0c08ec220cb8b3557305f494bdf1f3a12b344
AHMED AMINE GUIIDAT <50912308+AHMED-AMINE-GUIIDAT@users.noreply.github.com>
2021-09-02 22:39:34 +0100

    Update color.dart

--- sentiments_analysis/lib/utils/color.dart
+++ sentiments_analysis/lib/utils/color.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 
-//Color orangeColors = Color(0xFF0D47A1);
-//Color orangeLightColors = Color(0xFF64B5F6);
+
 Color orangeColors = Colors.white;
 Color orangeLightColors = Colors.white;
